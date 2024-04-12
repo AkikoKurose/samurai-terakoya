22c76ebb8092ed6a4d176faca4d7db6d7fd4525d
黒瀬晶子 <ak1723rock@gmail.com>
2024-04-12 22:21:36 +0900

    課題７提出

---
+++ php-basice-kadai/kadai_007/associative-array.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎_課題8</title>
+</head>
+<body>
+<?php
+$data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+print_r($data);
+?>
+</body>
+</html>
